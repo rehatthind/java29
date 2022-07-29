dacac619b35117e0369b4e4a38b782ff86f675c9
rehatthind <110219959+rehatthind@users.noreply.github.com>
2022-07-29 12:04:21 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rehatthind
+ */
+public class circle {
+    public static void main(String args[])
+    {
+    
+        double radius = 1.2,area,perimeter;
+        area= 3.14*radius*radius;
+        perimeter = 2 * radius*3.14 ;
+        System.out.println("area is "+area) ;
+        System.out.println("perimeter is "+perimeter) ;
+        
+    }
+
+}
